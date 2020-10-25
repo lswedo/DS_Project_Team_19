658b78b6ade1b6522cef00a10a74d7f660630003
natehopf-evil <70525401+natehopf-evil@users.noreply.github.com>
2020-10-25 15:34:08 -0400

    Update index.app.js
    Removed Firefighter info

--- app/public/js/index.app.js
+++ app/public/js/index.app.js
@@ -1,8 +1,7 @@
 var app = new Vue({
   el: '#OCFR_tables',
   data: {
-    certList: [],
-    ffList: []
+    certList: []
   },
     created() {
      this.get_certs();
@@ -13,15 +12,6 @@ var app = new Vue({
        console.log(json)}
      );
    },
-   created() {
-    this.get_ffs();
-    fetch("api/firefighters/")
-    .then( response => response.json() )
-    .then( json => {
-      this.ffList = json;
-      console.log(json)}
-    );
-  },
   methods: {
     get_certs: function() {
      fetch("api/certifications/")
@@ -32,16 +22,6 @@ var app = new Vue({
      );
    }
  },
-  methods: {
-    get_ffs: function() {
-     fetch("api/firefighters/")
-     .then( response => response.json() )
-     .then( json => {
-       this.ffList = json;
-       console.log(json)}
-     );
-   }
-  }
 })
 var app = new Vue({
   el: '#Certification_action',
@@ -134,103 +114,6 @@ var app = new Vue({
        console.log(this.updateForm);
        // TODO: test a result was returned!
        this.updatedCert = "Certification " + this.updateForm['CertId']+" updatedas: '" + this.updateForm['CertName'] + "' From Agency: " + this.updateForm['CertifyAgency']
-     });
-
-     console.log("Updating...!");
-   }
-  }
-})
-var app = new Vue({
-  el: '#Firefighter_action',
-  data: {
-    newffList: [],
-    newffForm: {},
-    new_ff: ''
-  },
-  created() {
-   fetch("api/firefighters/")
-   .then( response => response.json() )
-   .then( json => {
-     this.commentList = json;
-     console.log(json)}
-     );
-   },
-    methods: {
-      newffData() {
-        return {
-          user_ff: "",
-        }
-      },
-      handleNewff( evt ) {
-       fetch('api/firefighters/create.php', {
-         method:'POST',
-         body: JSON.stringify(this.newffForm),
-         headers: {
-           "Content-Type": "application/json; charset=utf-8"
-         }
-        })
-       .then( response => response.json() )
-       .then( json => {
-         console.log("Returned from post:", json[0]);
-         // TODO: test a result was returne
-         this.newffList.push(json[0]);
-         this.new_ff = "Person " + json[0]['PerId']+" submitted as: '" + json[0]['fname'] + "' From Agency: " + json[0]['CertifyAgency']
-       });
-
-       console.log("Creating (POSTing)...!");
-       console.log(this.newffForm);
-       console.log(this.newffList);
-
-       this.newffForm = this.newffData();
-     }
-    }
-  })
-var app = new Vue({
-  el: '#delete_entry_ff',
-  data: {
-    deleteForm_ff: {},
-    deletedff: ''
-  },
-  methods: {
-    deleteff( evt ) {
-     fetch('api/firefighters/delete.php', {
-       method:'POST',
-       body: JSON.stringify(this.deleteForm_ff),
-       headers: {
-         "Content-Type": "application/json; charset=utf-8"
-       }
-      })
-     .then( response => response.json() )
-     .then( json => {
-       console.log(this.deleteForm_ff);
-       // TODO: test a result was returned!
-       this.deletedff = "Person " + this.deleteForm_ff['PerId']+" Deleted"
-     });
-
-     console.log("Deleting...!");
-   }
-  }
-})
-var app = new Vue({
-  el: '#update_entry_ff',
-  data: {
-    updateForm_ff: {},
-    updatedff: ''
-  },
-  methods: {
-    updateff( evt ) {
-     fetch('api/firefighters/update.php', {
-       method:'POST',
-       body: JSON.stringify(this.updateForm_ff),
-       headers: {
-         "Content-Type": "application/json; charset=utf-8"
-       }
-      })
-     .then( response => response.json() )
-     .then( json => {
-       console.log(this.updateForm_ff);
-       // TODO: test a result was returned!
-       this.updatedff = "Person " + this.updateForm_ff['PerId']+" updatedas: '" + this.updateForm_ff['fName'] + "' From Agency: " + this.updateForm_ff['CertifyAgency']
      });
 
      console.log("Updating...!");
