07d2d9f493995bfaabc872f6548d306c5e626322
natehopf-evil <70525401+natehopf-evil@users.noreply.github.com>
2020-10-25 16:25:51 -0400

    Create create.php
    Update

---
+++ app/public/api/personel/create.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+
+$stmt = $db->prepare(
+  'INSERT INTO Per_Cert (PerId, CertId, CertDate)
+  VALUES (?,?,?)'
+);
+
+$stmt->execute([
+  $_POST['PerId'],
+  $_POST['CertId'],
+  $_POST['CertDate']
+]);
+
+$pk = $db->lastInsertId();
+
+header('HTTP/1.1 303 See Other');
+header('Location: ../personel/?PerId=' . $pk);
