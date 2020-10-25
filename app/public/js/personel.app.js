5eedc30ec250cdf0b43783c6d96ff7fa53c1bb06
JesusSantillanMinila <jesusant@iu.edu>
2020-10-25 02:04:28 -0400

    Index: html & php done. Needs js work

---
+++ app/public/js/personel.app.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: '#OCFR_tables',
+  data: {
+    perList: []
+  },
+  methods: {
+    get_certs: function() {
+     fetch("api/certifications/")
+     .then( response => response.json() )
+     .then( json => {
+       this.perList = json;
+       console.log(json)}
+     );
+    }
+  }
+})
+// var app = new Vue({
+//   el: '#OCFR_tables',
+//   data: {
+//
+//     perList: []
+//   },
+//     created() {
+//      this.get_certs();
+//      fetch("api/personel/")
+//      .then( response => response.json() )
+//      .then( json => {
+//        this.perList = json;
+//        console.log(json)}
+//      );
+//    }
+// })
