01731f69029980fd6b77cfa2408636764b8c1fc7
natehopf-evil <70525401+natehopf-evil@users.noreply.github.com>
2020-10-25 16:25:48 -0400

    Create delete.php
    Update

---
+++ app/public/api/personel/delete.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$stmt = $db->prepare(
+  $sql = 'DELETE FROM Per_Cert WHERE PerId = ? AND CertId = ? AND CertDate = ?'
+);
+
+$stmt->execute([
+  $_POST['PerId'],
+  $_POST['CertId'],
+  $_POST['CertDate']
+]);
+
+
+$message = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($message, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
