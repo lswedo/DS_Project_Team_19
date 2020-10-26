737d0bdee5ee59813e027d361fdf971c0be8d513
bengaither <70533526+bengaither@users.noreply.github.com>
2020-10-26 10:03:40 -0400

    EC2 Test

--- app/common.php
+++ app/common.php
@@ -2,7 +2,7 @@
 
 include 'vendor/autoload.php';
 
-// This block decodes POSTed JSON and adds it to $_POST for easier use
+// This block decodes POSTed JSON and adds it to $_POST for easier use 
 if (($_SERVER['REQUEST_METHOD'] ?? '') == 'POST'
 && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
   $_POST = json_decode(file_get_contents('php://input'), true);
